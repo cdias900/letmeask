0e00f22c1f016914bd85b4df69db82928452b2c9
Pedro Campos <pedro@cdias.dev>
2021-06-24 16:02:36 -0300

    Add Home Screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
+// import Home from './pages/Home';
+import NewRoom from './pages/NewRoom';
+
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <NewRoom />
   );
 }
 
